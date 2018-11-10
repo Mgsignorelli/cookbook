8e5975ade532994d10d2c31de4e5647d17f42458
mgsignorelli <m.signorelli@gmail.com>
2018-11-10 18:20:47 +0000

    setup orm for data clases

---
+++ models.py
@@ -0,0 +1,46 @@
+from pony.orm import *
+
+
+db = Database()
+
+
+class Allergy(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    ingredients = Set('Ingredient')
+
+
+class Category(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    recipes = Set('Recipe')
+    name = Required(str)
+
+
+class Recipe(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    user = Required('User')
+    categories = Set(Category)
+    title = Required(str)
+    method = Required(LongStr)
+    upvotes = Optional(int, unsigned=True)
+    downvotes = Optional(int, unsigned=True)
+    ingredients = Set('Ingredient')
+
+
+class Ingredient(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    allergies = Set(Allergy)
+    recipes = Set(Recipe)
+
+
+class User(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    recipes = Set(Recipe)
+    name = Required(str)
+    email = Required(str)
+    password = Required(str)
+
+
+db.bind(provider='sqlite', filename='database.sqlite')
+db.generate_mapping()
