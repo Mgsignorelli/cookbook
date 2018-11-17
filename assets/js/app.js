df5e3c5f709fb2949c20cc75b27da21be9114ff9
mgsignorelli <m.signorelli@gmail.com>
2018-11-17 15:42:06 +0000

    building assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,7 @@ window.Popper = require('popper.js').default;
 window.$ = require('jquery');
 require('bootstrap');
 
+$('.onClickSubmitDeleteRequest').on('click', (ev) => {
+    const $target = $(ev.currentTarget);
+    $target.parent().parent().find('form').submit();
+});
