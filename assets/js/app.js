0924f3ee4e68eb986dc6d2b0ca50c9895d95013f
micaela <m.signorelli@gmail.com>
2018-11-22 23:10:05 +0000

    Adds Select2 for allergy selector in ingredient edit
    * Installs Select2 as dependecny
    * Adds Select2 to Ingredient edit allergies
    * Removes commited precompiled assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 window.Popper = require('popper.js').default;
 window.$ = require('jquery');
 require('bootstrap');
+require('select2');
 
 $('.onClickSubmitDeleteRequest').on('click', (ev) => {
     const $target = $(ev.currentTarget);
     $target.parent().parent().find('form').submit();
 });
+
+$('.select2').select2();
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
 
-mix.js('assets/js/app.js', 'public/js')
-  .sass('assets/scss/app.scss', 'public/css/app.css')
+mix.js('assets/js/app.js', 'public')
+  .sass('assets/scss/app.scss', 'public/app.css')
   .copy('assets/images', 'public/images');
